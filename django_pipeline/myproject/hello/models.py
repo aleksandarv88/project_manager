992b12c39b969b70cd5ad9e0c35a87b8de85aa80
Aleksandar Volchev <aleksandar.volchev@gmail.com>
2025-09-20 08:19:05 +0200

    updates for the UI mostly, hit the limit for the Codex:(

--- django_pipeline/myproject/hello/models.py
+++
@@ -1,9 +0,0 @@
-from django.db import models
-
-class Item(models.Model):
-    name = models.CharField(max_length=100)
-    image = models.ImageField(upload_to='items/', blank=True, null=True)  # stores uploaded images in media/items/
-
-    def __str__(self):
-        return self.name
-
